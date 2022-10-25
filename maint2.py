a60be662791b84990f41645e61984ec68588321b
Ahujakaur <101709678+Ahujakaur@users.noreply.github.com>
2022-10-25 14:19:35 +0530

    Update maint2.py

--- maint2.py
+++ maint2.py
@@ -1,66 +1,51 @@
+from flask import Flask, request, jsonify
+#from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from flask import product
+
+db_connect = create_engine()
+app = Flask(__name__)
+api = Api(app)
 
 
-import server.wsgi as flask
-import server.grunner as gunicorn
-from flask import has_request_context, request
-from utils.cfgreader import EnvReader, BoolVar, StrVar
-import logging
-from flask.logging import default_handler
+class Employees(product):
+    def get(self):
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select * from employees") # This line performs query and returns json result
+        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+    
+    def post(self):
+        conn = db_connect.connect()
+        print(request.json)
+        LastName = request.json['proName']
+        
+        Fax = request.json['Fax']
+        Email = request.json['Email']
+        query = conn.execute("insert into product values(null,'{0}','{1}','{2}'".format(proName,Fax,Email
+                            ))
+        return {'status':'success'}
+
+    
+class Tracks(product):
+    def get(self):
+        conn = db_connect.connect()
+        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+    
+class Employees_Name(product):
+    def get(self, employee_id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from employees where EmployeeId =%d "  %int(Fax))
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
 
 
-class RequestFormatter(logging.Formatter):
-    def format(self, record):
-        if has_request_context():
-            record.corr = request.headers.get('fiware_correlator', None)
-            record.remote_addr = request.remote_addr
-            record.srv = request.headers.get('fiware-service', None)
-            if record.srv:
-                record.subserv = request.headers.get(
-                    'fiware-servicepath', '/')
-            else:
-                record.subserv = '/'
-            if len(request.data) > 0 and request.json and request.json['data']:
-                record.payload = request.json['data']
-            else:
-                record.payload = None
-        else:
-            record.corr = None
-            record.remote_addr = None
-            record.srv = None
-            record.subserv = None
-            record.payload = None
-
-        return super().format(record)
-
-
-formatter = RequestFormatter(
-    'time=%(asctime)s.%(msecs)03d | '
-    'level=%(levelname)s | corr=%(corr)s | from=%(remote_addr)s | '
-    'srv=%(srv)s | subserv=%(subserv)s | op=%(funcName)s | comp=%(name)s | '
-    'msg=%(message)s | payload=%(payload)s | '
-    'thread=%(thread)d  | process=%(process)d',
-    datefmt='%Y-%m-%d %I:%M:%S'
-)
-
-default_handler.setFormatter(formatter)
-
-
-def use_flask() -> bool:
-    env_var = BoolVar('USE_FLASK', False)
-    return EnvReader().safe_read(env_var)
-
-
-def setup():
-    r = EnvReader(log=logging.getLogger().debug)
-    level = r.read(StrVar('LOGLEVEL', 'INFO')).upper()
-    logger = logging.getLogger()
-    logger.setLevel(level)
-    logger.addHandler(default_handler)
+api.add_resource(proname, '/name') # Route_1
+api.add_resource(Tracks, '/tracks') # Route_2
+api.add_resource(pro_name, '/employees/<employee_id>') # Route_3
 
 
 if __name__ == '__main__':
-    setup()
-    if use_flask():  # dev mode, run the WSGI app in Flask dev server
-        flask.run()
-    else:            # prod mode, run the WSGI app in Gunicorn
-        gunicorn.run()
+     app.run()
